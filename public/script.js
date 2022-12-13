86dfdd7604e29ee286352b2555c56134d2f3decf
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-13 18:45:19 -0500

    Changed script.js. Deleted Google Maps Api since my free trial is about to run out haha. Moved getWeather fucntion from getWeather.js into script.js for easier naviagtion and usage. Successfully connected to OpenWeatherMap API. However, the free tier does not provide a lot of data apart from current weather (no hourly forecasts included). THinking of searching for better free weather APIS to use in this project.

--- public/script.js
+++ public/script.js
@@ -1,26 +1,9 @@
-const searchInput = document.querySelector('[data-search]')
-const searchBox = new google.maps.places.SearchBox(searchInput)
+/* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046} */
 
-/* addListener is a method exclusive to the Google Places API. These methods/variables are from the Places API documentation */
-searchBox.addListener('places_changed', () => {
-    const place = searchBox.getPlaces()[0]
-    if (place == null) return
-    /* latitude and longitude values, since thats what OpenWeather API needs to search a city */
-    const latitude = place.geometry.location.lat()
-    const longitude = place.geometry.location.lng()
+function getWeather (lat, lon, timezone) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3a12b0d3db019e926ae376649bb95046`)
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+}
 
-    /* calling the API using fetch */
-    fetch('/weather', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            latitude: latitude,
-            longitude: longitude
-        })
-    }).then(res => res.json()).then(data => {
-        setWeatherData(data, place.formatted_address)
-    })
-})
+getWeather(51.5085,-0.1257)
