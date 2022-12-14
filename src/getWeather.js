ea75c8c6f04dec1e2f77c5673e0445557eae552f
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-14 17:30:19 -0500

    Added webpack for bundling modules. Spent an eternity dealing with import/export bugs in VSCode. Some errors show up in VSCode (.then can't read properties of undefined) but webpack compiles code with no errors. Don;t knwo what to make of that, but API calls succesfully retrieve and post data to console so far. Might be a VSCode bug since those are dime a dozen on their linux version lol. Will start working on rendering data on screen using JS instead of console logging.

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,9 +1,7 @@
-/* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046} */
-
-export function getWeather (lat, lon, timezone) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046}')
+function getWeather(lat, long, timezone){
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}1&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=${timezone}`)
         .then((response) => response.json())
         .then((data) => console.log(data))
 }
 
-getWeather(51.5085,-0.1257)
+export { getWeather }
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { getWeather } from './getWeather.js'
+
+/* OpenWeatherAPI info */
+/* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046} */
+
+/* Second API OpenMeteo */
+/* https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=America%2FNew_York */
+
+/* Intl.DateTimeFormat() to automatically select current time zone */
+let time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+getWeather(51.5085,-0.1257,time_zone)
+    .then(res => {console.log(res.data)})
