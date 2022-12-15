c561bf5b20e12da57fd4f2ccd83403da84f8a69d
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-15 20:19:19 +0100

    Added webpack for bundling modules. Spent an eternity dealing with import/export bugs in VSCode. Some errors show up in VSCode (.then can't read properties of undefined) but webpack compiles code with no errors. Don;t knwo what to make of that, but API calls succesfully retrieve and post data to console so far. Might be a VSCode bug since those are dime a dozen on their linux version lol. Will start working on rendering data on screen using JS instead of console logging.

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,9 +1,8 @@
-/* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046} */
+async function getWeather(lat, long, timezone){
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}1&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=${timezone}`)
 
-export function getWeather (lat, lon, timezone) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046}')
-        .then((response) => response.json())
-        .then((data) => console.log(data))
+    const getWeatherData = await response.json()
+    console.log(getWeatherData)
 }
 
-getWeather(51.5085,-0.1257)
+export { getWeather }
