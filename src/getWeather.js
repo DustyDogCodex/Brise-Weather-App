62082093047a5ed906895573b3632bfdd707199c
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-17 20:19:19 -0500

    finallt resolved all the nonsensical git errors I was getting, don't know why or how my flow branched out by itself. Git push was succesful but it didn't update my remote repo?? what is going on hereeeeeee. Ended up just rewriting most of the recent code to fix it. Hopefully dont run into this issue again.

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,9 +1,10 @@
-/* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046} */
 
-export function getWeather (lat, lon, timezone) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046}')
-        .then((response) => response.json())
-        .then((data) => console.log(data))
+/* https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=America%2FNew_York */
+
+async function getWeather (lat, long, timezone) {
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=${timezone}`)
+    const getWeatherData = await response.json() 
+    console.log(getWeatherData)
 }
 
 export { getWeather }
