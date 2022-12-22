25a70c3b5133a563650c34ffbc93107f5b7e8f32
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-21 20:19:19 -0500

    added a weatherCodeGuide module to change returned weathercodes from API into a corresponding imae that will be displayed along with the data. Added renderDailyWeather function for rendering daily weather forecast from API data onto the screen in a humane readable format. Added more data-attributes to daily-weather-section in index.html to allow for easier manipulation using JS. ADded a dayTime formatter that converts time in seconds (returned by API) into corresponding day. Working on a function to render hourly weather forecast on screen.

--- src/dataRenderingOnScreen.js
+++ src/dataRenderingOnScreen.js
@@ -1,4 +1,8 @@
+import { weatherCodeGuide } from "./weatherCodeGuide"
+
 function renderCurrentWeatherData(current){
+    document.querySelector('[data-current-weather-icon]').classList.add('fa-solid', `${weatherCodeGuide(current.weatherIcon)}`, 'fa-6x')
+
     document.querySelector('[data-current-temp]').textContent = current.currentTemperature
     document.querySelector('[data-current-high]').textContent = current.high
     document.querySelector('[data-current-fl-high]').textContent = current.FLHigh
@@ -8,12 +12,35 @@ function renderCurrentWeatherData(current){
     document.querySelector('[data-current-precipitation]').textContent = current.precipitation
 }
 
+/* JS for updating the daily-weather-info section */
+const dailyWeatherArea = document.querySelector('[data-daily-area]')
+const dailyTempTemplate = document.getElementById('daily-temp-template')
+/* using DateTimeFormat method to convert the time returned by API in seconds into the correct day of the week. This time format will be decided based on the localtime format, which is why i've left the locales parameter undefined. This allows the days to be translated into the local language too. For example, if opened in France, it will display the days in French. */
+const timeToDayConverter = new Intl.DateTimeFormat(undefined, { weekday: "long" })
+
 function renderDailyWeatherData(daily){
-    document.querySelector('[data-current-temp]').textContent = current.currentTemperature
+    dailyWeatherArea.innerHTML = ''
+    daily.forEach(day => {
+        /* this copies the daily-temp-template and uses it for every day card that will be added to this section */
+        const dailyCard = dailyTempTemplate.content.cloneNode(true)
+        dailyCard.querySelector('[data-day-temp]').textContent = day.dayTemp
+        dailyCard.querySelector('[data-date]').textContent = timeToDayConverter.format(day.timeValue)
+        dailyCard.querySelector('[data-day-weather-icon]').classList.add('fa-solid', `${weatherCodeGuide(day.icon)}`, 'fa-3x')
+        console.log(weatherCodeGuide(day.icon))
+        dailyWeatherArea.append(dailyCard)
+    })
+}
+
+/* JS for rendering hourly weather data */
+const hourlyWeatherArea = document.querySelector('[data-hourly]')
+const hourlyTempTemplate = document.getElementById('hourly-data-row-template')
+function renderHourlyWeatherData(hourly){
+
 }
 
 function dataRenderingOnScreen({ current, daily, hourly }){
     renderCurrentWeatherData(current)
+    renderDailyWeatherData(daily)
 }
 
 export { dataRenderingOnScreen }
--- src/weatherCodeGuide.js
+++ src/weatherCodeGuide.js
@@ -2,22 +2,24 @@
 
 function weatherCodeGuide(weatherCode){
     if (weatherCode == 0){
-        return 'fa-solid fa-sun'
+        return 'fa-sun'
     } else if (weatherCode >= 1 && weatherCode <=3){
-        return 'fa-solid fa-cloud-sun'
+        return 'fa-cloud-sun'
     } else if (weatherCode == 45 || weatherCode == 48){
-        return 'fa-solid fa-smog'
+        return 'fa-smog'
     } else if (weatherCode >= 51 && weatherCode <= 55){
-        return 'fa-solid fa-cloud-rain'
+        return 'fa-cloud-rain'
     } else if (weatherCode >= 61 && weatherCode <= 67){
-        return 'fa-solid fa-cloud-showers-heavy'
+        return 'fa-cloud-showers-heavy'
     } else if (weatherCode >= 71 && weatherCode <= 77){
-        return 'fa-solid fa-snowflake'
+        return 'fa-snowflake'
     } else if (weatherCode >= 80 && weatherCode <= 82){
-        return 'fa-solid fa-cloud-sun-rain'
+        return 'fa-cloud-sun-rain'
     } else if (weatherCode == 85 || weatherCode == 86){
-        return 'fa-solid fa-snowflake'
+        return 'fa-snowflake'
     } else if (weatherCode >= 95){
-        return 'fa-solid fa-cloud-bolt'
+        return 'fa-cloud-bolt'
     }
 }
+
+export { weatherCodeGuide }
