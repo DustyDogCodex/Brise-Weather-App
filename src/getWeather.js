6f01f68bba6f23049359d69d283bacc37918d2b5
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-23 20:19:19 -0500

    added a blur filter for when API is loading data. JS will remove it when data is loaded. Project complete. YAYAYAYAYYAYYAgit add .!

--- src/getWeather.js
+++ src/getWeather.js
@@ -4,6 +4,7 @@
 async function getWeather (lat, long, timezone) {
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=${timezone}`)
     const getWeatherData = await response.json() 
+    document.querySelector('.loader').classList.remove('loader')
     return getWeatherData
 }
 
