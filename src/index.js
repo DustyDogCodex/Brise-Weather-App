a13dbef27a0f9dba9757f5a9730376686de61cfc
Varun Malaviya <varunmalaviya7@gmail.com>
2022-12-23 20:19:19 -0500

    added geolocation.getCurrentPosition to get users current location. Added a success and fail function which will respond to user allowing/denying location access. Added error messages for failed API data retrieval. WOrking on adding a loading screen for when JS is fetcing data.

--- src/index.js
+++ src/index.js
@@ -4,23 +4,30 @@ import { parseDailyWeatherData } from './parseDailyWeatherData.js'
 import { parseHourlyWeatherData } from './parseHourlyWeatherData.js'
 import { dataRenderingOnScreen } from './dataRenderingOnScreen.js'
 
-/* OpenWeatherAPI info */
-/* https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3a12b0d3db019e926ae376649bb95046} */
-
-/* Second API OpenMeteo */
-/* https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=America%2FNew_York */
-
 /* Intl.DateTimeFormat() to automatically select current time zone */
 let time_zone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
-getWeather(51.5085,-0.1257,time_zone)
-    .then( data => {
-        return {
-            current: (parseCurrentWeatherData(data)),
-            daily: (parseDailyWeatherData(data)),
-            hourly: (parseHourlyWeatherData(data))
-        }
-    })
-    .then( data => {
-        dataRenderingOnScreen(data)
-    })
+/* using nvaigator.geolocation for getting users current location */
+navigator.geolocation.getCurrentPosition(currentLocationSuccess, currentLocationFailed)
+
+function currentLocationSuccess({ coords }){
+    getWeather(coords.latitude,coords.longitude,time_zone)
+        .then( data => {
+            return {
+                current: (parseCurrentWeatherData(data)),
+                daily: (parseDailyWeatherData(data)),
+                hourly: (parseHourlyWeatherData(data))
+            }
+        })
+        .then( data => {
+            dataRenderingOnScreen(data)
+        })
+        .catch(e => {
+            console.log(e)
+            alert("Oh no! Looks like our API has been snowed in! Please try again or check console for error information!")
+        })
+}
+
+function currentLocationFailed(){
+    alert("Looks like a storm is interuppting communication with you! Please share your location so we can share the current weather data at your location!")
+}
